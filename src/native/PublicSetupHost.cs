954c107c27e194619bb695c5ee3fdb8731952a89
coachedai <298694791+coachedai@users.noreply.github.com>
2026-09-21 13:11:21 +0200

    Add invariant culture dependency for Setup recovery journal

--- src/native/PublicSetupHost.cs
+++ src/native/PublicSetupHost.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
